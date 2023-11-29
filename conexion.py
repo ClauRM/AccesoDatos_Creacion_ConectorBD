0c8d6d229de23621696faff63998edebe94e6478
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-11-29 19:43:22 +0100

    Definición de una clase sgbd con método constructor y método insert

--- conexion.py
+++ conexion.py
@@ -1,15 +1,20 @@
 import subprocess
 
-operacion = "insert"
-basededatos = "miempresa"
-coleccion = "clientes1"
-documento = "clientes4"
-contenido = "Cometin Sonico"
+class sgbd:
+    #constructor
+    def __init__(self,basededatos):
+        self.basededatos = basededatos
+    #metodos
+    def insert(self):
+        self.operacion = "insert"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
 
-comando = '"C:\\Users\\claud\\OneDrive\\Documentos\\IMF\\2 DAM\\Acceso a datos\\Prácticas\\005-Creación de un conector de BD\\sgbd.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        comando = '"C:\\Users\\claud\\OneDrive\\Documentos\\IMF\\2 DAM\\Acceso a datos\\Prácticas\\005-Creación de un conector de BD\\sgbd.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
-if resultado.returncode == 0:
-    print("ok")
-else:
-    print("ko")
+        if resultado.returncode == 0:
+            return("ok insert")
+        else:
+            return("ko insert")
