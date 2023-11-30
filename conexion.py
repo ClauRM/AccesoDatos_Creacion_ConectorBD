8e1f0e1be2cd6ec569795fa672e57ee24d3d2b3d
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-11-30 11:11:45 +0100

    Extracción de la clase Sgbd a otro fichero sgbd_conector.py

--- conexion.py
+++ conexion.py
@@ -1,23 +1,6 @@
-import subprocess
-
-class Sgbd:
-    #constructor
-    def __init__(self,basededatos):
-        self.basededatos = basededatos
-    #metodos
-    def insert(self,coleccion,documento,contenido):
-        self.operacion = "insert"
-        self.coleccion = coleccion
-        self.documento = documento
-        self.contenido = contenido
-
-        comando = '"C:\\Users\\claud\\OneDrive\\Documentos\\IMF\\2 DAM\\Acceso a datos\\Prácticas\\005-Creación de un conector de BD\\sgbd.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+contenido+'"'
-        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-
-        if resultado.returncode == 0:
-            return("ok insert")
-        else:
-            return("ko insert")
+from sgbd_conector import Sgbd #importar la clase Sgbd del fichero sgbd_conector.py
 
 Conexion1 = Sgbd("miempresa") #objeto conexion
-Conexion1.insert("clientes1","cliente5","Leonardo Sanchez")
+Conexion1.insert("clientes1","cliente6","Oscar Mayer")
+
+##min31
---
+++ sgbd_conector.py
@@ -0,0 +1,20 @@
+import subprocess
+
+class Sgbd:
+    #constructor
+    def __init__(self,basededatos):
+        self.basededatos = basededatos
+    #metodos
+    def insert(self,coleccion,documento,contenido):
+        self.operacion = "insert"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
+
+        comando = '"C:\\Users\\claud\\OneDrive\\Documentos\\IMF\\2 DAM\\Acceso a datos\\Prácticas\\005-Creación de un conector de BD\\sgbd.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+        if resultado.returncode == 0:
+            return("ok insert")
+        else:
+            return("ko insert")
